cb24591e5777e7520ebb20daf9255c2a1411b347
Noel Sebastian Marquez Tovar <noel@MacBook-Pro-de-Noel-3.local>
2026-05-07 14:31:04 -0600

    revert: deshacer merge PR #11 feature/setup-miguel

--- src/main/java/com/farmacov/application/usecase/CreateSintomaGraveUseCase.java
+++ src/main/java/com/farmacov/application/usecase/CreateSintomaGraveUseCase.java
@@ -1,14 +1,11 @@
 package com.farmacov.application.usecase;
 
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import com.farmacov.application.dto.CreateSintomaGraveDto;
 import com.farmacov.domain.models.SintomaGrave;
 import com.farmacov.domain.repository.SintomaGraveRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.NotFoundException;
-
-import java.util.List;
 
 @ApplicationScoped
 public class CreateSintomaGraveUseCase {
@@ -20,7 +17,6 @@ public class CreateSintomaGraveUseCase {
         this.sintomaGraveRepository = sintomaGraveRepository;
     }
 
-    @Transactional
     public SintomaGrave execute(CreateSintomaGraveDto dto){
         SintomaGrave sintomaGrave = new SintomaGrave();
 
@@ -28,14 +24,5 @@ public class CreateSintomaGraveUseCase {
         sintomaGrave.setNombre(dto.getNombre());
 
         return sintomaGraveRepository.save(sintomaGrave);
-    }
-
-    public SintomaGrave getById(Integer id) {
-        return sintomaGraveRepository.getById(id)
-                .orElseThrow(() -> new NotFoundException("Sintoma grave con id " + id + " no encontrado"));
-    }
-
-    public List<SintomaGrave> getAll() {
-        return sintomaGraveRepository.getAll();
     }
 }
